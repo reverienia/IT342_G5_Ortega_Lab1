64272c7a9b82d71c19079e526ee6bb217c396a7e
Shania <ortegakhryz04@gmail.com>
2026-02-17 16:24:31 +0800

    Initial commit: set up springboot and react project with dependencies

---
+++ src/main/java/com/sysinteg/jotify/JotifyApplication.java
@@ -0,0 +1,13 @@
+package com.sysinteg.jotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotifyApplication.class, args);
+	}
+
+}
