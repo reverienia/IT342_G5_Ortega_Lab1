458610b2a0a7652259cbdabdd82f8cf2c608afc8
Shania <ortegakhryz04@gmail.com>
2026-02-17 19:12:45 +0800

    feat: making user java files such as entity, repo and service

--- backend/src/main/java/com/sysinteg/jotify/repository/UserRepository.java
+++ backend/src/main/java/com/sysinteg/jotify/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.sysinteg.jotify.repository;
 
-public class UserRepository {
+import com.sysinteg.jotify.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
