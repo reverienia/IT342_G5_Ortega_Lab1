2cdfec7118f11a10bfc7df441d2dc3f57e593025
Shania <ortegakhryz04@gmail.com>
2026-02-17 20:49:32 +0800

    feat: made API layer, configured SQL and BCrypt Bean

--- backend/src/main/java/com/sysinteg/jotify/security/SecurityConfig.java
+++ backend/src/main/java/com/sysinteg/jotify/security/SecurityConfig.java
@@ -1,4 +1,13 @@
 package com.sysinteg.jotify.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
